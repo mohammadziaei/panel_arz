bae662ba6e302b3d482c5c6c764df1980e014f5a
mohammadziaei <ziaeimohammad10@gmail.com>
2024-11-08 10:45:46 +0330

    initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,14 @@
+
+// collapse sidabar in lg size 
+
+// document.querySelector('aside .triger button').addEventListener('click', function() {
+//     const sidebar = document.getElementById('sidebar');
+//     const textElements = sidebar.querySelectorAll('span');
+
+//     // تغییر بین col-2 و col-1
+//     sidebar.classList.toggle('col-2');
+//     sidebar.classList.toggle('col-1');
+
+//     // مخفی‌کردن یا نمایش متن‌ها
+//     textElements.forEach(text => text.classList.toggle('d-none'));
+// });
